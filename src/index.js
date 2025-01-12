187a6fa85d601dd3cab47c75262d5f214fac7667
SithPrincess <Cassie2727@gmail.com>
2025-01-11 20:13:00 -0600

    Update index.js
    api key and url added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+function cityInput(city) {
+  let apiKey = "355o03d48211t632342b7f4c029b7a0f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  console.log(apiUrl);
+}
+
 function searchSubmit(Event) {
   Event.preventDefault();
   let searchInput = document.querySelector("#search-input");
@@ -6,35 +12,3 @@ function searchSubmit(Event) {
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
-
-function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let day = date.getDay();
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let formattedDay = days[day];
-  return `${formattedDay} ${hours}:${minutes}`;
-}
-
-let currentDateElement = document.querySelector(`#current-date`);
-let currentDate = new Date();
-
-currentDateElement.innerHTML = formatDate(currentDate);
