1cc564eb2df6205268fffc3a3c0b76acb5243b3d
SithPrincess <Cassie2727@gmail.com>
2025-01-11 20:19:21 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+function updateWeatherDetails(response) {
+  console.log(response.data.temperture.current);
+}
+
 function cityInput(city) {
   let apiKey = "355o03d48211t632342b7f4c029b7a0f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
