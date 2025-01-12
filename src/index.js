047ee93237c3a0f53468abc4cb5d73ceb4a1bf92
SithPrincess <Cassie2727@gmail.com>
2025-01-12 00:01:59 -0600

    Upadate
    Updated search api

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 function updateWeatherDetails(response) {
-  let temperatureCurrent = document.querySelector("#current-temperature");
+  let temperatureCurrent = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
+  let cityCurrent = document.querySelector("#current-city");
+  cityCurrent.innerHTML = response.data.city;
   temperatureCurrent.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
   let apiKey = "355o03d48211t632342b7f4c029b7a0f";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(updateWeatherDetails);
 }
 
-function searchSubmit(Event) {
-  Event.preventDefault();
+function searchSubmit(event) {
+  event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityCurrent = document.querySelector("#current-city");
-  cityCurrent.innerHTML = searchInput.value;
   searchCity = searchInput.value;
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Gatlinburg");
